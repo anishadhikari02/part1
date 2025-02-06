4218a8acb4dea50514f0e503e6e3cb6ef5349cef
anishadhikari02 <anishadhikari1000@gmail.com>
2025-02-06 18:24:31 +0545

    Part 1 completed

--- part1.py
+++ part1.py
@@ -7,16 +7,70 @@ print(f"The perimeter of rectangle is {2*(a+b)}.")
 
 """
 
-# 2)  
+# 2)Python is great,it's wild!
 
 """
 print("Python is great,it's wild!")
 
 """
 
-# 3)
+# 3) 2 to the 10th power
 
 '''
 print(2**10)
+
+'''
+
+# 4) 7 factorial minus 5 factorial
+'''
+import math
+
+output = math.factorial(7) - math.factorial(5)
+print(output)
+
+'''
+
+# 5) your forename multiplied by 5
+
+'''
+print(5*"anish")
+
+'''
+
+#6) your name left justified 15 space
+
+'''
+print("Anish".ljust(15))
+
+'''
+
+# 7) PI to 5 decimal place
+
+'''
+print(f"{22/7:.5f}")
+
+'''
+
+# 8) 200 modulus 12
+
+'''
+print(200 % 12)
+
+'''
+
+# 9) 7.2 as an integer value
+
+'''
+a=7.2
+print(int(a))
+
+'''
+
+# 10) The unicode encoding for your name
+
+'''
+
+print(ord('A'), ord('n'), ord('i'), ord('s'), ord('h'))
+
 
 '''
